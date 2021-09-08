eb8a029602969fd449a3b213604cf9eda68e76b2
pscott <scott.piriou@ledger.fr>
2021-09-08 11:01:04 +0200

    Remove unused printf

--- src/provide_parameters.c
+++ src/provide_parameters.c
@@ -289,10 +289,6 @@ static void handle_megaswap(ethPluginProvideParameter_t *msg, paraswap_parameter
 void handle_provide_parameter(void *parameters) {
     ethPluginProvideParameter_t *msg = (ethPluginProvideParameter_t *) parameters;
     paraswap_parameters_t *context = (paraswap_parameters_t *) msg->pluginContext;
-    PRINTF("eth2 plugin provide parameter %d %.*H\n",
-           msg->parameterOffset,
-           PARAMETER_LENGTH,
-           msg->parameter);
 
     msg->result = ETH_PLUGIN_RESULT_OK;
 
