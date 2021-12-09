c5923ee868626876328d4f7881ac55529cfe1f94
Alexandre Paillier <alexandre.paillier@ledger.fr>
2021-12-09 16:14:50 +0100

    Now uses the SDK's copy_parameter function

--- src/handle_provide_parameter.c
+++ src/handle_provide_parameter.c
@@ -1,23 +1,15 @@
 #include "paraswap_plugin.h"
 
-// Copies the whole parameter (32 bytes long) from `src` to `dst`.
-// Useful for numbers, data...
-static void copy_parameter(uint8_t *dst, size_t dst_len, uint8_t *src) {
-    // Take the minimum between dst_len and parameter_length to make sure we don't overwrite memory.
-    size_t len = MIN(dst_len, PARAMETER_LENGTH);
-    memcpy(dst, src, len);
-}
-
 // Copy amount sent parameter to amount_sent
 static void handle_amount_sent(const ethPluginProvideParameter_t *msg,
                                paraswap_parameters_t *context) {
-    copy_parameter(context->amount_sent, sizeof(context->amount_sent), msg->parameter);
+    copy_parameter(context->amount_sent, msg->parameter, sizeof(context->amount_sent));
 }
 
 // Copy amount sent parameter to amount_received
 static void handle_amount_received(const ethPluginProvideParameter_t *msg,
                                    paraswap_parameters_t *context) {
-    copy_parameter(context->amount_received, sizeof(context->amount_received), msg->parameter);
+    copy_parameter(context->amount_received, msg->parameter, sizeof(context->amount_received));
 }
 
 static void handle_beneficiary(const ethPluginProvideParameter_t *msg,
